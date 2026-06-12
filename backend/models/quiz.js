45c94bd3b26acbecfda0a26ada00bf582a1a5bec
24-hk <hkalaikovan@gmail.com>
2026-06-12 15:10:53 +0100

    Create quiz.js

---
+++ backend/models/quiz.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+    userId:         { type: String, required: true },
+    subject:        String,
+    score:          Number,
+    totalQuestions: Number,
+    completedAt:    { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Quiz', quizSchema);
